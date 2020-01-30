ac1a95ca3b6a3b51534f2aebc4aad5fcb0c4fc18
xatkx <xatkx1@gmail.com>
2020-01-29 22:56:10 -0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+import React, { Component } from 'react';
+//crear componente navbar/midle/end
+import Navbar from './componets/top/navbar'
+
+
+
+class App extends Component {
+
+  state = {
+    videos: []
+  }
+
+  searchApi = (search) => {
+    const api = `https://pixabay.com/api/videos/?key=14901812-5f44c61e4696aa53c5c4721f5&q=${search}`
+    fetch(api)
+      .then(video => video.json())
+      .then(produc => {
+        this.setState({
+          videos: produc.hits
+        },
+          console.log(this.state.videos)
+        )
+      })
+
+  }
+
+
+  render(){
+    return(
+    <div className='row row-cols-1'>
+
+      <div className='col' >
+        
+        <Navbar searchApi={this.searchApi} />
+        
+      </div>
+      <div className='col'>
+        <div className=''>
+          <h1>luego vere para que sera la pagina</h1>
+          <h1>luego vere para que sera la pagina</h1>
+          <h1>luego vere para que sera la pagina</h1>
+          <h1>luego vere para que sera la pagina</h1>
+          <h1>luego vere para que sera la pagina</h1>
+          <h1>luego vere para que sera la pagina</h1>
+        </div>
+      </div>
+
     </div>
-  );
+    )
+  }
 }
 
 export default App;
